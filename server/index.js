a1c4eba9fc3974bb008d5db83d9dd7d0b945ee2d
Kaijun <Demonbane.cn@gmail.com>
2016-03-15 15:03:31 +0100

    clean up

--- server/index.js
+++ server/index.js
@@ -8,40 +8,36 @@ const SERVER_PORT = 3001
 
 const app = express()
 
+/*
+  Development Env: enable the HotReplacementPugin dynamically
+  Production Env: Set the Server's static path to the webpack build(output) path.
+*/
 if(app.get('env') === 'development'){
-
-  //enable HotReplacementPlugin
   webpackConfig.entry.jsx = [webpackConfig.entry.jsx]
   webpackConfig.entry.jsx.unshift(`webpack-dev-server/client?http://localhost:${CLIENT_PORT}/`, "webpack/hot/dev-server")
   webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
-
   const devServer= new WebpackDevServer(webpack(webpackConfig), {
-    contentBase: __dirname + '/../client', //seems doesn't effect
+    contentBase: __dirname + '/../client', 
     hot: true,
     historyApiFallback: true,
-
-    //proxy the server port, so that easy to avoid cross-domain while requesting the APIs
+    //proxy the server port, easy to avoid cross-domain while requesting the APIs
     proxy: {
-      "/api/*": `http://localhost:${SERVER_PORT}`
+      "/api/*": `http://localhost:${SERVER_PORT}`,
+      // "*": `http://localhost:${SERVER_PORT}`
     },
-
-    // webpack-dev-middleware options
     stats: { colors: true },
   }).listen(CLIENT_PORT)
-
 }
 else{
+  // enable history-api-fallback
   app.use(history())
   app.use(express.static(__dirname + '/../static'))
 }
 
-app.get('/test', function (req, res) {
-  res.send('Hello World!')
-});
 app.get('/api/test', function (req, res) {
-  res.send('Hello World!')
+  res.send('The APIs works!')
 });
 
 app.listen(SERVER_PORT, function () {
-  console.log(`Example app listening on port ${SERVER_PORT}!`)
+  console.log(`API Server listening on port ${SERVER_PORT}!`)
 });
