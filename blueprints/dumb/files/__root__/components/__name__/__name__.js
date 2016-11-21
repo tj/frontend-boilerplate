af338f2566b713dd6bef5c036d2d5ed9863240a9
Dominik Schmidt <evolveeee@gmail.com>
2016-11-21 11:58:23 +0100

    remove import of `Component` in dump blueprint

--- blueprints/dumb/files/__root__/components/__name__/__name__.js
+++ blueprints/dumb/files/__root__/components/__name__/__name__.js
@@ -1,5 +1,5 @@
 
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import styles from './<%= pascalEntityName %>.css'
 
 const <%= pascalEntityName %> = props => (
