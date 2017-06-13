febb0ac885adfd6ab38de14682d71328c62c95b0
Nick Schmidt <nick@kreativgebiet.com>
2017-06-13 14:11:19 +0200

    rename fast-click initializing function

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import initializeFastClick from 'react-fastclick';
 
 import App from './pages';
 import configure from './modules/store';
@@ -10,6 +11,7 @@ import history from './modules/history';
 
 const store = configure();
 fastclick();
+initializeFastClick();
 
 ReactDOM.render(
   <Provider store={store}>
