9949c0f725c15d04716997ae7511efa86960c9fc
Nick Schmidt <nick@kreativgebiet.com>
2017-06-13 15:29:28 +0200

    add loading state for button

--- client/components/Button/index.js
+++ client/components/Button/index.js
@@ -1,24 +1,69 @@
+// @flow
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import Spin from 'react-spin';
 import { Link } from 'react-router-dom';
 import has from 'lodash/has';
+import invariant from 'invariant';
+import classnames from 'classnames/bind';
+
+import { spinConfig as defaultSpinConfig } from '../../config.js';
 import styles from './style.css';
 
-export default function Button(props) {
+const cx = classnames.bind(styles);
+
+export default function Button({ loading, spinConfig, ...props }: Object) {
   let Tag = 'button';
 
   if (has(props, 'to')) {
     Tag = Link;
+
+    delete props.href;
+    delete props.onClick;
   } else if (has(props, 'href')) {
     Tag = 'a';
+
+    delete props.to;
+    delete props.onClick;
   }
 
   return (
     <Tag
-      className={styles.main}
+      disabled={loading}
+      className={cx('main', { 'main--loading': loading })}
       {...props}
     >
-      {props.children}
+      {/* Implement the spinner for loading activity */}
+      <span className={styles.main__spinner}>
+        <Spin stopped={!loading} config={{ ...defaultSpinConfig, ...spinConfig }} />
+      </span>
+
+      {/* Implement the children (content) of the button */}
+      <span className={styles.main__content}>
+        {props.children}
+      </span>
     </Tag>
   );
 }
+
+Button.defaultProps = {
+  spinConfig: defaultSpinConfig,
+  loading: false,
+  to: null,
+  href: null,
+  onClick: () =>
+    invariant(false, 'No `onClick`, `to` or `href` was defined. Used fallback, with a noop.'),
+};
+
+Button.propTypes = {
+  to: PropTypes.string,
+  loading: PropTypes.bool,
+  href: PropTypes.string,
+  onClick: PropTypes.func,
+  spinConfig: PropTypes.object,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
--- client/config.js
+++ client/config.js
@@ -3,3 +3,26 @@ export const helmetDefaults = {
   title: 'Home',
   titleTemplate: 'Boilerplate - %s',
 };
+
+export const spinConfig = {
+  lines: 13,
+  length: 4,
+  width: 3,
+  radius: 7,
+  scale: 0.75,
+  corners: 1,
+  color: '#fff',
+  opacity: 0.15,
+  rotate: 0,
+  direction: 1,
+  speed: 0.8,
+  trail: 50,
+  fps: 20,
+  zIndex: 2e9,
+  className: 'spinner',
+  top: '50%',
+  left: '50%',
+  shadow: false,
+  hwaccel: false,
+  position: 'absolute',
+};
