914c67ae03c2c774640e123a04edb59554f6f85d
Dominik Schmidt <evolveeee@gmail.com>
2017-10-09 15:09:09 +0200

    update paths to put `static` folder in root

--- config/paths.js
+++ config/paths.js
@@ -4,6 +4,6 @@ const path = require('path');
 module.exports = {
   appBuild: path.join(__dirname, '..', 'client'),
   appPublic: path.join(__dirname, '..', 'client'),
-  outputPath: path.join(__dirname, './static'),
+  outputPath: path.join(__dirname, '..', 'static'),
   outputName: 'bundle.js',
 };
